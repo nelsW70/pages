ea42679be742fa9f8a17e60c9951dd63b2fee61f
Nelson Foltz <nelsfoltz@gmail.com>
2020-01-25 15:45:11 -0500

    adding image url to images array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'At the beach',
+      url:
+        // tslint:disable-next-line: max-line-length
+        'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Doggo',
+      url:
+        'https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Mustard looking stuff',
+      url:
+        'https://images.unsplash.com/photo-1556802675-63920fcd7c7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Walrus on the  beach',
+      url:
+        // tslint:disable-next-line: max-line-length
+        'https://images.unsplash.com/photo-1460899960812-f6ee1ecaf117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    }
+  ];
 }
